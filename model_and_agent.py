96f158973c995a77cd008f284c974575a19a146a
PrioteasaAndrei <57443744+PrioteasaAndrei@users.noreply.github.com>
2022-09-02 21:33:43 +0300

    Update model_and_agent.py

--- model_and_agent.py
+++ model_and_agent.py
@@ -1,4 +1,5 @@
 
+from turtle import distance
 import matplotlib.pyplot as plt
 import numpy as np
 from game import BLOCK_SIZE, SnakeGame
@@ -30,103 +31,152 @@ def prepare_enviroment(game):
 
     if apple[0] % BLOCK_SIZE != 0.0 or apple[1] % BLOCK_SIZE != 0.0:
         raise Exception()
+   
+   ##  dictionary 
+   
+   ## key : str(state)
+    qtable = {}
 
-    ## 32 * 24 and 3 actions
-    qtable = np.zeros((32,32,32,32,3))
-    # qtable = np.zeros((768,3))
+    ## populate qtable 
+
+    boolean_vals = [0,1]
+
+    ## -> 2 ^ 12 states = 4096 states which is ok
+    states = []
+
+    for s1 in boolean_vals:
+        for s2 in boolean_vals:
+            for s3 in boolean_vals:
+                for s4 in boolean_vals:
+                    for s5 in boolean_vals:
+                        for s6 in boolean_vals:
+                            for s7 in boolean_vals:
+                                for s8 in boolean_vals:
+                                    for s9 in boolean_vals:
+                                        for s10 in boolean_vals:
+                                            for s11 in boolean_vals:
+                                                states.append(np.array([s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11]))
+
+
+    ## 1110011110101 : [0.5,1.2,3.3]
+    ## key is state : val for 100 010 and 001
+    for state in states:
+        qtable[str(state)] = np.array([0,0,0],dtype=float)    
     
-    ## rewards
-    ## bigger by 2 set the bounderies equal to -10
-    rewards = np.zeros((24,32))
+
+    ## Use experience learning for better results
+    ## history will be set to [] after every match
+    history = []
+
+    return qtable,history
 
 
-    ## reward for apple
-    ## are they reverted ??/
-    x_OX =  int(apple[0] // BLOCK_SIZE)
-    y_OY = int(apple[1] // BLOCK_SIZE)
-    ## ASTA E COORDONATA FARA PADDING
-    rewards[y_OY][x_OX] = 10
 
-    ## make the snake tail -10
-
-    ## am inversat si aici pentru ca x sunt coloanele si y liniile
-    ## SI ASTEA LA FEL FARA PADDING SUNT COORDONATELE
-    for square in snake[1:]:
-        rewards[int(square[1] // BLOCK_SIZE)][int(square[0] // BLOCK_SIZE)] = -10
-
-
-    rewards = np.pad(rewards,pad_width=1,mode='constant',constant_values=-10)
-    
-    return qtable,rewards
-
-
-# [danger_straight,danger_left,danger_right,
-# moving_left,moving_right,moving_up,moving_down,
-# food_left,food_right,food_up,food_down]
-def get_state_complex(self, game):
-        head = game.snake[0]
-
-        ## w.r.t the direction of motion
-
-        left  = Point(head.x - 20, head.y)
-        right  = Point(head.x + 20, head.y)
-        up  = Point(head.x, head.y - 20)
-        down  = Point(head.x, head.y + 20)
-        
-        dir_l = game.direction == Direction.LEFT
-        dir_r = game.direction == Direction.RIGHT
-        dir_u = game.direction == Direction.UP
-        dir_d = game.direction == Direction.DOWN
-
-        state = [
-            # Danger straight
-            (dir_r and game.is_collision(right)) or 
-            (dir_l and game.is_collision(left)) or 
-            (dir_u and game.is_collision(up)) or 
-            (dir_d and game.is_collision(down)),
-
-            # Danger right
-            (dir_u and game.is_collision(right)) or 
-            (dir_d and game.is_collision(left)) or 
-            (dir_l and game.is_collision(up)) or 
-            (dir_r and game.is_collision(down)),
-
-            # Danger left
-            (dir_d and game.is_collision(right)) or 
-            (dir_u and game.is_collision(left)) or 
-            (dir_r and game.is_collision(up)) or 
-            (dir_l and game.is_collision(down)),
-            
-            # Move direction
-            dir_l,
-            dir_r,
-            dir_u,
-            dir_d,
-            
-            # Food location 
-            game.food.x < game.head.x,  # food left
-            game.food.x > game.head.x,  # food right
-            game.food.y < game.head.y,  # food up
-            game.food.y > game.head.y  # food down
-            ]
-
-        return np.array(state, dtype=int)
-
-
-## X SI Y SUNT INVERSATE  ??? 
-## X e coloana y e linia
 def get_state_easy(game):
     return Point(int(game.head.y // BLOCK_SIZE),int(game.head.x // BLOCK_SIZE))
 
-def get_state_relative_to_apple(game):
+'''
+
+    returns a len 11 boolean array 
+
+'''
+def get_state( game):
     head = game.snake[0]
-    apple = game.food
-    tail = game.snake[-1]
+    point_l = Point(head.x - 20, head.y)
+    point_r = Point(head.x + 20, head.y)
+    point_u = Point(head.x, head.y - 20)
+    point_d = Point(head.x, head.y + 20)
+    
+    dir_l = game.direction == Direction.LEFT
+    dir_r = game.direction == Direction.RIGHT
+    dir_u = game.direction == Direction.UP
+    dir_d = game.direction == Direction.DOWN
 
-    apple_relative = Point(apple.x - head.x, apple.y - head.y)
-    tail_relative = Point(tail.x - head.x, tail.y - head.y)
+    state = [
+        # Danger straight
+        (dir_r and game.is_collision(point_r)) or 
+        (dir_l and game.is_collision(point_l)) or 
+        (dir_u and game.is_collision(point_u)) or 
+        (dir_d and game.is_collision(point_d)),
 
-    return np.array([int(apple_relative.y // BLOCK_SIZE),int(apple_relative.x // BLOCK_SIZE),int(tail_relative.y // BLOCK_SIZE),int(tail_relative.x // BLOCK_SIZE)])
+        # Danger right
+        (dir_u and game.is_collision(point_r)) or 
+        (dir_d and game.is_collision(point_l)) or 
+        (dir_l and game.is_collision(point_u)) or 
+        (dir_r and game.is_collision(point_d)),
+
+        # Danger left
+        (dir_d and game.is_collision(point_r)) or 
+        (dir_u and game.is_collision(point_l)) or 
+        (dir_r and game.is_collision(point_u)) or 
+        (dir_l and game.is_collision(point_d)),
+        
+        # Move direction
+        dir_l,
+        dir_r,
+        dir_u,
+        dir_d,
+        
+        # Food location 
+        game.food.x < game.head.x,  # food left
+        game.food.x > game.head.x,  # food right
+        game.food.y < game.head.y,  # food up
+        game.food.y > game.head.y  # food down
+        ]
+
+    return np.array(state, dtype=int)
+
+
+
+
+def distance_to_apple(x_food,y_food,x_head,y_head):
+    return np.abs(x_head - x_food) + np.abs(y_head - y_food)
+    
+
+## reason == True -> snake died
+## reason == False -> snake still alive / caught an apple 
+def update_qtable(qtable,reason):
+    reverse_history = history[::-1]
+    for i,h in enumerate(reverse_history[:-1]):
+        if reason == True:
+            ## snake died
+            h_state = reverse_history[0]['state']
+            h_action = reverse_history[0]['action']
+            action_index = action_to_index(h_action)
+            reward = -1
+            qtable[str(h_state)][action_idx] = alpha * qtable[str(h_state)][action_idx] + ( 1 - alpha) * reward
+        else:
+            ## current state
+            current_state = h['state']
+            current_food = h['apple']
+            current_head = h['head']
+
+            ## prev state
+            prev_state = reverse_history[i+1]['state']
+            prev_act = reverse_history[i+1]['action']
+            prev_food = reverse_history[i+1]['apple']
+            prev_head = reverse_history[i+1]['head']
+
+            ## if snake ate food or is closer reward is 1
+            if prev_food != current_food:
+                reward = 1
+            ## works in px because we only do division for conversion
+            elif distance_to_apple(current_food[0],current_food[1],current_head[0],current_head[1]) < distance_to_apple(prev_food[0],prev_food[1],prev_head[0],prev_head[1]):
+                reward = 1
+            else:
+                ## further from the food
+                reward = -1
+
+            qtable[str(prev_state)][action_to_index(prev_act)] = alpha * (qtable[str(prev_state)][action_to_index(prev_act)]) + (1-alpha) * (reward + gamma * np.max(qtable[str(current_state)]))
+
+
+def action_to_index(action):
+    if np.array_equal(np.array([1,0,0]),action):
+        return 0
+    elif np.array_equal(np.array([0,1,0]),action):
+        return 1
+    elif np.array_equal(np.array([0,0,1]),action):
+        return 2
 
 '''
 Use a epsilon greedy alg to allow for exploration of new paths that at first seem not to get a good future reward
@@ -143,88 +193,71 @@ def get_action(epsilon,old_state):
 
     move = [0,0,0]
 
-    ## if qtable is empty for that state do a random action
-    if np.random.random() > epsilon or np.array_equal(qtable[old_state[0],old_state[1],old_state[2],old_state[3]],np.array([0,0,0,0])):
+    if np.random.random() > epsilon:
          ## get random action
+        print("HERERERER")
         move_idx = random.randint(0, 2)
         move[move_idx] = 1
     else:
         ## get the best action
         ## should take the max of the array and make it 1
-        ## dar daca e negativ ?? sa iau in modul
-        print(np.argmax(qtable[old_state[0],old_state[1]]))
-        print(qtable[old_state[0],old_state[1]])
+        arr = qtable[str(old_state)]
+        move[np.argmax(arr)] = 1
 
-        move[np.argmax(qtable[old_state[0],old_state[1]])] = 1
+    history.append({
+    'state': old_state,
+    'action': move,
+    ## in px coord
+    'apple': (GAME.food.x,GAME.food.y),
+    ## in px coord
+    'head': (GAME.head.x,GAME.head.y)
+    })
 
     return move
 
 
 GAME = SnakeGame()
-EPISODES = 500
-qtable,rewards = prepare_enviroment(GAME)
+EPISODES = 150
+qtable,history = prepare_enviroment(GAME)
 
 ## learning rate
-alpha = 0.5
+alpha = 0.1
 
 ## discount rate is big because we want long term rewards
-gamma = 1.0  
+gamma = 0.9
 
 ## percentage of time we take the best action considering the qtable
 epsilon = 0.9
-
-snake = GAME._get_snake_idxs()
-apple = GAME._get_food()
-
-
 
 iteration_for_plotting  = []
 score_for_plotting = []
 
 for i in range(EPISODES):
-    ## jocul abia a pornit la linia 107
     game_over = False
     GAME.reset()
+    history = [] 
     while not game_over:
 
-        old_state = get_state_easy(GAME)
-        ## ? problema aici ?? 
+        old_state = get_state(GAME)
         action = get_action(epsilon,old_state)
+
         reward,game_over,score = GAME.play_step(action)
-        new_state = get_state_easy(GAME)
 
-        print("Reward is:",reward)
-        (m,n,p) = np.shape(qtable)
-        
-        if new_state[0] == m or new_state[1] == n:
-            game_over = True
-            break
+        new_state = get_state(GAME)
 
-        ## sau mai usor fac eu corelarea direct
         '''
             action == [1,0,0] -> 0 
             action == [0,1,0] -> 1
             action == [0,0,1] -> 2
 
         '''
+        action_idx = action_to_index(action)
+        update_qtable(qtable,game_over)
 
-        action_idx = 0 
-
-        if np.array_equal(action,np.array([1,0,0])):
-            action_idx = 0
-        elif np.array_equal(action,np.array([0,1,0])):
-            action_idx = 1
-        elif np.array_equal(action,np.array([0,0,1])):
-            action_idx = 2
-        else:
-            raise Exception()
-
-
-        qtable[old_state[0],old_state[1],action_idx] += alpha * (reward + gamma * np.max(qtable[new_state[0],new_state[1]]) - qtable[old_state[0],old_state[1],action_idx])
-        
     iteration_for_plotting += [i]
     score_for_plotting += [score] 
 
+
 plot_score(iteration_for_plotting,score_for_plotting)
 np.save('qtable.npy',qtable)
-    
+
